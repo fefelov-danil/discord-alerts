fb3c77347373445799f1ff097c04394ee82e04b0
Daniil Fefelov <fefelov.danil@gmail.com>
2023-11-07 10:07:09 +0400

    api deploy

--- src/pages/api/deploy.ts
+++ src/pages/api/deploy.ts
@@ -1,16 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
 
-type Data = {
-  name: string
-}
+const webhookUrl = 'https://discord.com/api/webhooks/1169657472638795906/lfizjRATT-xJ7B2KcCa-OWGdrCNZCwYVrbkb3ngsBZLzyawxnNZQt3A4tQCevnrmVy5m';
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
 
   if (req.method === 'POST') {
     res.status(200).json(req.body)
+
+    const data = {
+      content: req.body.message
+    }
+
+    axios.post(webhookUrl, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error('Ошибка при отправке сообщения:', error);
+      });
   } else {
     res.status(200).json({ name: 'John Doe' })
   }
